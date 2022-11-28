46d726ef274123efc15e9a9e39779690a29fdc8e
Sergio Matias <sergio.matias@janeladigital.com>
2022-11-28 11:43:18 +0000

    Update Markers

--- docs/marker-recorder.js
+++ docs/marker-recorder.js
@@ -1,157 +1,170 @@
-// Map definitions
-let config = {
-	minZoom: 7,
-	maxZoom: 18,
+var MapRouting = (function ()
+{
+	var map = null;
+	var points = new Array();
+	var lineGroup = L.layerGroup();
+	var marker = null;
+
+	var _MapRouting = {
+
+		init: function(lat, lng, zoomLevel)
+		{
+			// Map definitions
+			let config = {
+				minZoom: 7,
+				maxZoom: 18,
+			};
+
+			map = L.map("map", config).setView([lat, lng], zoomLevel);
+
+			L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				attribution:
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}).addTo(map);
+
+			// Set the inicial marker
+			map
+				.locate({
+					setView: true,
+					enableHighAccuracy: true,
+				})
+				// with the location found create the marker
+				.on("locationfound", (e) => {
+					console.log(e);
+			
+					// marker
+					marker = L.marker([e.latitude, e.longitude]).bindPopup(
+						"nou"
+					);
+					
+					// add marker
+					map.addLayer(marker);
+					
+					// point to list
+					points.push([e.latitude, e.longitude ])
+					document.getElementById("log").innerHTML += "new marker (" + e.latitude + "," + e.longitude + ")<br> ";
+				})
+				// On error do alert
+				.on("locationerror", (e) => {
+					console.log(e);
+				});
+		}, 
+
+		updateMarker: function() 
+		{
+			map
+				.locate({
+					setView: true,
+					enableHighAccuracy: true,
+				})
+				// with the location found create the marker
+				.on("locationfound", (e) => {
+					console.log(e);
+
+					// marker
+					marker.setLatLng(new L.LatLng(e.latitude, e.longitude));
+				})
+				// On error do alert
+				.on("locationerror", (e) => {
+					console.log(e);
+				});
+		},
+
+		Start: function()
+		{
+			MapRouting.addPoint();
+
+			document.getElementById("start").setAttribute('data-run','1');
+			document.getElementById("log").innerHTML += "Started<br> ";
+
+			setInterval(function()
+			{
+				MapRouting.addPoint();
+				MapRouting.updateMarker();
+			}, 30000);
+
+			document.getElementById("start").style.display = "none";
+			document.getElementById("stop").style.display = "block";
+		},
+
+		Stop: function() 
+		{
+			clearInterval();
+			MapRouting.addPoint();
+			document.getElementById("start").removeAttribute('data-run');
+			document.getElementById("log").innerHTML += "Stopped<br> ";
+			
+			document.getElementById("start").style.display = "block";
+			document.getElementById("stop").style.display = "none";
+		},
+
+		addPoint: function  ()
+		{
+			if (document.getElementById("start").hasAttribute("data-run")) 
+			{
+		
+				map
+				.locate({
+					setView: true,
+					enableHighAccuracy: true,
+				})
+				// with the location found create the marker
+				.on("locationfound", (e) => {
+					console.log(e);
+		
+					/*
+					// marker
+					const marker = L.marker([e.latitude, e.longitude]).bindPopup(
+						"nou"
+					);
+					
+					// add marker
+					map.addLayer(marker);
+					*/
+					
+					// point to list
+					points.push([e.latitude, e.longitude ])
+					document.getElementById("log").innerHTML += "new marker (" + e.latitude + "," + e.longitude + ")<br> ";
+				})
+				// On error do alert
+				.on("locationerror", (e) => {
+					console.log(e);
+				});
+		
+				MapRouting.showRoute();
+			}
+		},
+		
+		showRoute: function () 
+		{	
+			// Clean map layer
+			lineGroup.removeFrom(map);
+		
+			// Add new map layer
+			lineGroup = L.layerGroup()
+		
+			// Add poligns
+			lineGroup.addLayer(L.polyline(points, {
+				color: "red",
+				opacity: 0.5,
+				weight: 20,
+			}));
+			
+			lineGroup.addTo(map);
+		}
 };
+
+	return _MapRouting;
+
+})();
+
 
 // tart point os the map
 const zoom = 18;
 const lat = 39.08940255014147;
 const lng = -8.209767767093245;
 
-// setting the map
-const map = L.map("map", config).setView([lat, lng], zoom);
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-	attribution:
-		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-var points = new Array();
-var lineGroup = L.layerGroup();
-var marker = null;
-
-map
-	.locate({
-		setView: true,
-		enableHighAccuracy: true,
-	})
-	// with the location found create the marker
-	.on("locationfound", (e) => {
-		console.log(e);
-
-		// marker
-		marker = L.marker([e.latitude, e.longitude]).bindPopup(
-			"nou"
-		);
-		
-		// add marker
-		map.addLayer(marker);
-		
-		// point to list
-		points.push([e.latitude, e.longitude ])
-		document.getElementById("log").innerHTML += "new marker (" + e.latitude + "," + e.longitude + ")<br> ";
-	})
-	// On error do alert
-	.on("locationerror", (e) => {
-		console.log(e);
-	});
-
-UpdateMarker();
-
-
-
-function Start()
-{
-	AddPoint();
-	document.getElementById("start").setAttribute('data-run','1');
-	document.getElementById("log").innerHTML += "Started<br> ";
-
-	setInterval(function()
-	{
-		AddPoint();
-		UpdateMarker();
-	}, 30000);
-
-	document.getElementById("start").style.display = "none";
-	document.getElementById("stop").style.display = "block";
-}
-
-function Stop() 
-{
-	clearInterval();
-	AddPoint();
-	document.getElementById("start").removeAttribute('data-run');
-	document.getElementById("log").innerHTML += "Stopped<br> ";
-	
-	document.getElementById("start").style.display = "block";
-	document.getElementById("stop").style.display = "none";
-}
-
-function UpdateMarker() 
-{
-	map
-		.locate({
-			setView: true,
-			enableHighAccuracy: true,
-		})
-		// with the location found create the marker
-		.on("locationfound", (e) => {
-			console.log(e);
-
-			// marker
-			marker.setLatLng(new L.LatLng(e.latitude, e.longitude));
-		})
-		// On error do alert
-		.on("locationerror", (e) => {
-			console.log(e);
-		});
-}
-
-function AddPoint ()
-{
-	if (document.getElementById("start").hasAttribute("data-run")) 
-	{
-
-		map
-		.locate({
-			setView: true,
-			enableHighAccuracy: true,
-		})
-		// with the location found create the marker
-		.on("locationfound", (e) => {
-			console.log(e);
-
-			/*
-			// marker
-			const marker = L.marker([e.latitude, e.longitude]).bindPopup(
-				"nou"
-			);
-			
-			// add marker
-			map.addLayer(marker);
-			*/
-			
-			// point to list
-			points.push([e.latitude, e.longitude ])
-			document.getElementById("log").innerHTML += "new marker (" + e.latitude + "," + e.longitude + ")<br> ";
-		})
-		// On error do alert
-		.on("locationerror", (e) => {
-			console.log(e);
-		});
-
-		ShowRoute();
-	}
-}
-
-function ShowRoute() 
-{	
-	// Clean map layer
-	lineGroup.removeFrom(map);
-
-	// Add new map layer
-	lineGroup = L.layerGroup()
-
-	// Add poligns
-	lineGroup.addLayer(L.polyline(points, {
-		color: "red",
-		opacity: 0.5,
-		weight: 20,
-	  }));
-	
-	lineGroup.addTo(map);
-}
+MapRouting.init(lat, lng, zoom);
 
 
 /*
