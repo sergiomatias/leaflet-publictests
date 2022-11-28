60029eca686cebdd130aec72ad20b5567aee6f02
Sergio Matias <sergio.matias@janeladigital.com>
2022-11-28 10:01:41 +0000

    Try to record route

--- docs/marker-recorder.js
+++ docs/marker-recorder.js
@@ -17,7 +17,38 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-map
+var lineGroup = L.layerGroup()
+var markerGroup = L.layerGroup()
+
+UpdateMarker();
+
+
+var points = new Array();
+
+function Start()
+{
+	AddPoint();
+	document.getElementById("start").setAttribute('data-run','1');
+	document.getElementById("log").innerHTML += "Started<br> ";
+
+	setInterval(function()
+	{
+		AddPoint();
+		UpdateMarker();
+	}, 30000);
+}
+
+function Stop() 
+{
+	clearInterval();
+	AddPoint();
+	document.getElementById("start").removeAttribute('data-run');
+	document.getElementById("log").innerHTML += "Stopped<br> ";
+}
+
+function UpdateMarker() 
+{
+	map
 	.locate({
 		setView: true,
 		enableHighAccuracy: true,
@@ -30,19 +61,23 @@ map
 			"nou"
 		);
 		
+		markerGroup.removeFrom(map);
+
 		// add marker
-		map.addLayer(marker);
+		markerGroup.addLayer(marker);
+		markerGroup.addTo(map);
 	})
 	// On error do alert
 	.on("locationerror", (e) => {
 		console.log(e);
 		
 	});
+}
 
-
-setInterval(function()
+function AddPoint ()
 {
-	map
+	if (document.getElementById("start").hasAttribute("data-run")) {
+		map
 		.locate({
 			setView: true,
 			enableHighAccuracy: true,
@@ -50,6 +85,8 @@ setInterval(function()
 		// with the location found create the marker
 		.on("locationfound", (e) => {
 			console.log(e);
+
+			/*
 			// marker
 			const marker = L.marker([e.latitude, e.longitude]).bindPopup(
 				"nou"
@@ -57,11 +94,59 @@ setInterval(function()
 			
 			// add marker
 			map.addLayer(marker);
-			document.getElementById("log").innerHTML += "new marker (" + e.latitude + "," + e.longitude + ")<br> ";
+			*/
 			
+			// point to list
+			points.push([e.latitude, e.longitude ])
+			document.getElementById("log").innerHTML += "new marker (" + e.latitude + "," + e.longitude + ")<br> ";
 		})
 		// On error do alert
 		.on("locationerror", (e) => {
 			console.log(e);
 		});
-}, 60000);
+	}
+}
+
+function showRoute () 
+{	
+	// Clean map layer
+	lineGroup.removeFrom(map);
+
+	// Add new map layer
+	lineGroup = L.layerGroup()
+
+	// Add poligns
+	lineGroup.addLayer(L.polyline(points, {
+		color: "red",
+		opacity: 0.5,
+		weight: 20,
+	  }));
+	
+	lineGroup.addTo(map);
+}
+
+
+/*
+
+// define array of points to use for line
+const points = [
+	[52.2308124251888, 21.011003851890568],
+	[52.2302604393307, 21.01121842861176],
+	[52.2297445891999, 21.011282801628116],
+	[52.22953759032849, 21.011492013931278],
+	[52.22954416173605, 21.01194798946381],
+	[52.22967558968336, 21.012285947799686],
+	[52.2300008721797, 21.012935042381287],
+	[52.230306438414374, 21.014378070831302],
+  ];
+  
+  // add polyline to map
+  L.polyline(points, {
+	color: "red",
+	opacity: 0.5,
+	weight: 20,
+  })
+	.bindPopup("polygon")
+	.addTo(map);
+
+*/
